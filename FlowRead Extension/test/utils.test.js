a92e502d6c0e4a9b9b90cee866432c25c5b46d72
Hossein Montazeri <h.montaz.ca@gmail.com>
2025-07-02 10:44:40 -0700

    update

---
+++ FlowRead Extension/test/utils.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+// const { normalizeHost } = require('../src/scripts/utils.cjs.js');
+const { normalizeHost } = require('../src/scripts/utils.js');
+
+describe('normalizeHost', () => {
+    it('trims whitespace', () => {
+        assert.strictEqual(normalizeHost('  www.example.com  '), 'example.com');
+    })
+
+    it('removes http:// prefix', () => {
+        assert.strictEqual(normalizeHost('http://www.example.com'), 'example.com');
+        assert.strictEqual(normalizeHost('https://www.example.com'), 'example.com');
+    })
+
+    it('removes www and lowercases', () => {
+        assert.strictEqual(normalizeHost('WWW.Google.com'), 'google.com');
+    });
+
+    it('keeps non-www domains intact', () => {
+        assert.strictEqual(normalizeHost('sub.example.org'), 'sub.example.org');
+    });
+
+    it('removes query parameters', () => {
+        assert.strictEqual(normalizeHost('https://www.example.com/path/to/resource?query=param'), 'example.com/path/to/resource');
+    });
+
+    it('returns empty string unchanged', () => {
+        assert.strictEqual(normalizeHost(''), '');
+    });
+});
